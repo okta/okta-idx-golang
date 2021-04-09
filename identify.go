8a0342d1923f5f3ef1f5b4934ef2c68fc5461d4b
bogdanprodan-okta <bogdan.prodan@okta.com>
2021-04-09 21:39:37 +0300

    Fixed set password

--- identify.go
+++ identify.go
@@ -3,6 +3,7 @@ package idx
 import (
 	"context"
 	"fmt"
+	"strings"
 )
 
 type IdentifyRequest struct {
@@ -20,4 +21,17 @@ func (r *Response) Identify(ctx context.Context, ir *IdentifyRequest) (*Response
                 "rememberMe": %t
             }`, ir.Identifier, ir.RememberMe))
 	return ro.Proceed(ctx, identify)
+}
+
+func (r *Response) SetPasswordOnLogin(ctx context.Context, password string) (*Response, error) {
+	ro, err := r.remediationOption("challenge-authenticator")
+	if err != nil {
+		return nil, err
+	}
+	credentials := []byte(`{
+				"credentials": {
+					"passcode": "` + strings.TrimSpace(password) + `"
+				}
+			}`)
+	return ro.Proceed(ctx, credentials)
 }
